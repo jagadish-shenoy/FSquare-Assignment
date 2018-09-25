a81d3c0a11b31a7388c84385b2f94ffbcce0696c
jagadish.shenoy <jagadish.shenoy@clarity.com>
2018-09-25 21:02:08 +0200

    Initial commit just added the first activity

---
+++ app/src/main/java/abnamroassignment/com/abnamroassignment/VenueSearchActivity.kt
@@ -0,0 +1,12 @@
+package abnamroassignment.com.abnamroassignment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class VenueSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_venue_search)
+    }
+}
